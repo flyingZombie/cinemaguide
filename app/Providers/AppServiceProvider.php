b2efc3c75cec1a459b92997de6ee6a2d32b2fc98
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-08 09:43:24 +0000

    add auth control

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,4 +25,14 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class  => \App\Policies\UserPolicy::class,
+    ];
 }
