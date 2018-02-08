636c1389025e798812dcfd0e56aace2d40b37570
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-08 15:32:06 +0000

    user can register using api

---
+++ app/Http/Requests/Api/UserRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Dingo\Api\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'password' => 'required|string|min:6',
+        ];
+    }
+
+}
